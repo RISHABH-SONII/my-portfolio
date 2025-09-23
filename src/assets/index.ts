31c1754943aa2384feef28fb4b283e764c7a44fb
RISHABH-SONII <rishabhsoni50414@gmail.com>
2025-09-23 18:54:11 +0530

    working on herosection

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,10 +3,12 @@ import heroImage from './heroImage.jpg';
 import headerLogo from './unit_conv_logoweb.webp';
 import headerLogoImage1 from './headerLogoImage1.png';
 import headerLogoImage2 from './headerLogoImage2.png';
+import braces from './brace.png';
 
 export const Images = {
   heroImage,
   headerLogo,
   headerLogoImage1,
   headerLogoImage2,
+  braces,
 };
--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -1,51 +1,3 @@
-// 'use client';
-
-// import { useEffect, useState } from 'react';
-// import classes from './HeroSection.module.css';
-// import { ClassNames } from '@emotion/react';
-// import Image from 'next/image';
-// import { Images } from '@/assets';
-
-// export default function HeroSection() {
-//   const [tagline, setTagline] = useState('');
-//   const fullTagline = 'I build stunning web experiences with React & Next.js.';
-
-//   // Typing effect
-//   useEffect(() => {
-//     let index = 0;
-//     const interval = setInterval(() => {
-//       setTagline(fullTagline.slice(0, index));
-//       index++;
-//       if (index > fullTagline.length) clearInterval(interval);
-//     }, 80);
-//     return () => clearInterval(interval);
-//   }, []);
-
-//   return (
-//     <section className={classes.heroSection}>
-//       <div className={classes.textColumn}>
-//         <p className={classes.welcomeText}>👋 WELCOME TO MY PORTFOLIO</p>
-//         <h1 className={classes.nameText}>
-//           Rishabh <span className={classes.nameText1}>Soni</span>
-//         </h1>
-//         <h2 className={classes.techBackground}>Flutter Dev</h2>
-//         <p className={classes.taglineText}>{tagline}</p>
-//       </div>
-
-//       <div className={classes.imageColumn}>
-//         <Image
-//           src={Images.heroImage.src}
-//           alt="heroImage"
-//           width={400}
-//           height={400}
-//           className={classes.heroImage}
-//           priority
-//         />
-//       </div>
-//     </section>
-//   );
-// }
-
 'use client';
 
 import { useEffect, useState } from 'react';
@@ -73,9 +25,13 @@ export default function HeroSection() {
       <div className={classes.textColumn}>
         <p className={classes.welcomeText}>WELCOME TO MY PORTFOLIO 👋 </p>
         <h1 className={classes.nameText}>
-          Rishabh <br/><span className={classes.nameText1}>Soni</span>
+          Rishabh <br />
+          <span className={classes.nameText1}>Soni</span>
         </h1>
-        <h2 className={classes.techBackground}>Flutter Dev</h2>
+        <h2 className={classes.techBackground}>
+          <Image src={Images.braces} alt="curly braces" height={25} width={25} />
+          Flutter Dev
+        </h2>
         <p className={classes.taglineText}>{tagline}</p>
       </div>
 
